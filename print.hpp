24c466172583e143dc8ee4f740d8b8420383e998
Mashpoe <mashpoesemail@gmail.com>
2019-07-12 14:46:24 -0600

    Create print.hpp

---
+++ print.hpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+template <class Item> int printItem(Item item) {
+	std::cout << item << ' ';
+	return 0;
+}
+
+template <class ...Items>
+void unpackItems(Items...) {}
+
+template <class... Items> void print(Items... items) {
+	unpackItems(printItem<Items>(items)...);
+	std::cout << std::endl;
+}
